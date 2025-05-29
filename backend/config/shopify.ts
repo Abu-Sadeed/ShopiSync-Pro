c4b6983401c80385087129b5ad461a170a9046dc
Sadeed <abusadeed@iut-dhaka.edu>
2025-05-29 16:56:10 +0600

    Add Shopify configuration and update package dependencies
    - Implement Shopify API configuration in shopify.ts
    - Update package.json to include necessary devDependencies
    - Modify package-lock.json to reflect changes in dependencies

---
+++ backend/config/shopify.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import Shopify from 'shopify-api-node';
+dotenv.config();
+
+if (!process.env.SHOPIFY_SHOP || !process.env.SHOPIFY_ACCESS_TOKEN) {
+	throw new Error(
+		'Missing Shopify credentials! Check SHOPIFY_SHOP and SHOPIFY_ACCESS_TOKEN in your .env file.',
+	);
+}
+
+const shopify = new Shopify({
+	shopName: process.env.SHOPIFY_SHOP!,
+	accessToken: process.env.SHOPIFY_ACCESS_TOKEN!,
+});
+
+export default shopify;
