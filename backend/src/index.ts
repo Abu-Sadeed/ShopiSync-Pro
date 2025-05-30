8df284b9450badac52ab616962c233bac6821d93
Sadeed <abusadeed@iut-dhaka.edu>
2025-05-30 14:34:06 +0600

    feat: add CORS support to the Express application and update package dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import pool from './config/db';
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT ?? 4000;
+app.use(cors());
 app.use('/api', webhookRoutes);
 app.use(express.json());
 
